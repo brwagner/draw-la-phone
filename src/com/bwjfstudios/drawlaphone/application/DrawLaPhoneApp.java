18d7c7db1df17c6329bf88c7300a483089e73b4c
brwagner <brajswagner@gmail.com>
2014-09-05 12:15:46 -0400

    security
    put api keys in separate resource file that can't be accessed by people
    who view the repository.

--- src/com/bwjfstudios/drawlaphone/application/DrawLaPhoneApp.java
+++ src/com/bwjfstudios/drawlaphone/application/DrawLaPhoneApp.java
@@ -2,20 +2,18 @@ package com.bwjfstudios.drawlaphone.application;
 
 import android.app.Application;
 
+import com.bwjfstudios.drawlaphone.R;
 import com.parse.Parse;
 import com.parse.ParseACL;
 
 public class DrawLaPhoneApp extends Application {
-
-    private static final String APPLICATION_ID = "YOUR_KEY_HERE";
-    private static final String CLIENT_KEY = "YOUR_KEY_HERE";
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         // Add your initialization code here
-        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+        Parse.initialize(this, getString(R.string.APPLICATION_ID), getString(R.string.CLIENT_KEY));
 
         // If you would like all objects to be private by default, remove this line.
         ParseACL defaultACL = new ParseACL();
